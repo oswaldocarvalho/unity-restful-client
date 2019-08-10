e6ad1ff5d725f053220375a07a1a9a2797895bae
Oswaldo Carvalho Filho <oswaldo.vix@gmail.com>
2019-08-10 18:20:06 -0300

    demo api server

--- Assets/_user/scripts/Global.cs
+++ Assets/_user/scripts/Global.cs
@@ -3,8 +3,7 @@ using RESTClient.Responses;
 
 public static class Global
 {
-    // public const string API_URL = "http://basic-account-api.reptilaw.com";
-    public const string API_URL = "http://localhost";
+    public const string API_URL = "http://basic-account-api.reptilaw.com";
     public static SignInResponse Me;
     public static TokenAuthRestClient RestClient;
 }
